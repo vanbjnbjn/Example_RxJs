00601f9ee8fc9d33134541ab9c68228b7c8b2cb9
Nguyen Minh Tri <tri_nm09@care-connect.vn>
2023-11-27 10:51:43 +0700

    modify ui

--- webExampleRxJs/src/app/app.module.ts
+++ webExampleRxJs/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './layouts/menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs'
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatListModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webExampleRxJs/src/app/layouts/menu/menu.component.ts
+++ webExampleRxJs/src/app/layouts/menu/menu.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent implements OnInit {
 
+  menus = [
+    { id: 1, text: 'Overview', children:[
+      {id: 2, text: 'Observable'},
+      {id: 3, text: 'Operators'},
+      {id: 4, text: 'Subscripton'},
+      {id: 5, text: 'Subjects'},
+      {id: 6, text: 'Observable'},
+      {id: 7, text: 'Observable'},
+    ]},
+    {id: 8, text: 'Installation'}
+  ];
+  panelOpenState: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
